172b25a5d286f5f62305458f1ba086d1633af06e
joe <joel.pereda@ripio.com>
2022-12-21 03:01:05 -0300

    add translation

--- pages/_document.js
+++ pages/_document.js
@@ -4,16 +4,16 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="Vitalik Twitter Thread GPT-3 Writer" key="title" />
+        <meta property="og:title" content="Sermon generator" key="title" />
         <meta
           property="og:description"
-          content="Vitalik twitter thread generator
+          content="Input a text or verse and get a sermon
 "
           key="description"
         />
         <meta
           property="og:image"
-          content="https://www.cronista.com/files/image/330/330339/60953d1321301.jpg"
+          content="https://www.cronista.com/files/image/330/330339/60953d1321301.jpghttps://www.spurgeongems.org/2020/wp-content/uploads/2019/12/The-Ministers-Farewell-image-865x1024.jpg"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,18 @@
-import i18n from "i18next";
 import React, { useState, useEffect } from "react";
-import { useTranslation } from "react-i18next";
-import { loadTranslations } from "./api/i18next";
+import useTranslation from "next-translate/useTranslation";
 
 const Home = () => {
   const [userInput, setUserInput] = useState("");
-  const { t } = useTranslation();
   const [language, setLanguage] = useState("en");
+  const { t } = useTranslation("common");
 
-  const changeLanguage = async (lng) => {
-    // setLanguage(lng);
-    console.log("lng :>> ", lng);
-    await i18n.changeLanguage(lng).then(() => setLanguage(lng));
-  };
-  const initTranslations = async () => {
-    await loadTranslations(language);
+  const changeLanguage = (language) => {
+    setLanguage(language);
+    window.location.replace("/" + language);
   };
 
   useEffect(() => {
-    initTranslations();
+    setLanguage(window.location.pathname.split("/")[1]);
   }, []);
 
   const onUserChangedText = (event) => {
@@ -47,77 +41,76 @@ const Home = () => {
     setApiOutput(`${output.text}`);
     setIsGenerating(false);
   };
+
   return (
-    <>
-      {!language ? (
-        <div className="root">
-          <div className="container"></div>
+    <div className="root">
+      <div className="container">
+        <div className="header">
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignSelf: "flex-end",
+            }}
+          >
+            <a className="button" onClick={() => changeLanguage("es")}>
+              <p style={language === "es" ? { fontWeight: "bold" } : null}>
+                ES
+              </p>
+            </a>
+            <p>/</p>
+            <a className="button" onClick={() => changeLanguage("en")}>
+              <p style={language === "en" ? { fontWeight: "bold" } : null}>
+                EN
+              </p>
+            </a>
+          </div>
+          <div className="header-title">
+            <h1>{t("title")}</h1>
+          </div>
+          <div className="header-subtitle">
+            <h2>{t("subtitle")}</h2>
+          </div>
         </div>
-      ) : (
-        <div className="root">
-          <div className="container">
-            <div className="header">
-              <div style={{ display: "flex", flexDirection: "row", alignSelf: 'flex-end' }}>
-                <a className="button" onClick={() => changeLanguage("es")}>
-                  <p style={language === "es" ? { fontWeight: "bold" } : null}>
-                    ES
-                  </p>
-                </a>
-                <p>/</p>
-                <a className="button" onClick={() => changeLanguage("en")}>
-                  <p style={language === "en" ? { fontWeight: "bold" } : null}>
-                    EN
-                  </p>
-                </a>
+        {/* Add this code here*/}
+        <div className="prompt-container">
+          <textarea
+            className="prompt-box"
+            placeholder={t("placeholder")}
+            value={userInput}
+            onChange={onUserChangedText}
+          />
+          <div className="prompt-buttons">
+            <a
+              className={
+                isGenerating ? "generate-button loading" : "generate-button"
+              }
+              onClick={callGenerateEndpoint}
+            >
+              <div className="generate">
+                {isGenerating ? (
+                  <span className="loader"></span>
+                ) : (
+                  <p>{t("generate")}</p>
+                )}
               </div>
-              <div className="header-title">
-                <h1>{t("title")}</h1>
-              </div>
-              <div className="header-subtitle">
-                <h2>{t("subtitle")}</h2>
-              </div>
-            </div>
-            {/* Add this code here*/}
-            <div className="prompt-container">
-              <textarea
-                className="prompt-box"
-                placeholder={t("placeholder")}
-                value={userInput}
-                onChange={onUserChangedText}
-              />
-              <div className="prompt-buttons">
-                <a
-                  className={
-                    isGenerating ? "generate-button loading" : "generate-button"
-                  }
-                  onClick={callGenerateEndpoint}
-                >
-                  <div className="generate">
-                    {isGenerating ? (
-                      <span className="loader"></span>
-                    ) : (
-                      <p>{t("generate")}</p>
-                    )}
-                  </div>
-                </a>
-              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+      {apiOutput && (
+        <div className="output">
+          <div className="output-header-container">
+            <div className="output-header">
+              <h3>{t("Output")}</h3>
             </div>
           </div>
-          {apiOutput && (
-            <div className="output">
-              <div className="output-header-container">
-                <div className="output-header">
-                  <h3>{t("Output")}</h3>
-                </div>
-              </div>
-              <div className="output-content">
-                <p>{apiOutput}</p>
-              </div>
-            </div>
-          )}
+          <div className="output-content">
+            <p>{apiOutput}</p>
+          </div>
         </div>
       )}
-    </>
+    </div>
   );
 };
 
